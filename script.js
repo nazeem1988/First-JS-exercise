8f1c4690bffb4fed54d0ad1b2745289443b6bdc9
Zee Rohilla <rohillanazeem@gmail.com>
2019-07-07 22:56:29 -0500

    JS STALKER SOLUTION

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-var userName = prompt("What is your name?");
-alert("Nice to meet you, " + userName);
-console.log("Also great to meet, " + userName);
+var userOneName = prompt("What is your First name?");
+var userOneLast = prompt("What is your Last name?");
+var userOneAge = prompt("What is your Age?");
+console.log("Your full name is " + userOneName + " " + userOneLast);
+console.log("You are " + userOneAge + " years old");
